bc2669555a0ba5707c46ed5db547c472321cbf7a
TFGOFFICIAL <thomgerber03@gmail.com>
2022-10-20 13:58:36 +0200

    v2.0.0

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { UserAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom'
+import logo from './../assets/icons/LOGO_WHITE.png'
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -20,14 +21,15 @@ export default function SignIn() {
     }
   }
   return (
-    <div className='w-full h-[100vh] flex flex-col items-center justify-center'>
-        <div className='w-[98vw] lg:w-[400px] bg-white border shadow-xl flex flex-col items-center'>
+    <div className='bg-hero w-full h-[100vh] flex flex-col items-center justify-center'>
+        <img alt='' className='absolute w-10 top-5 left-5' src={logo}/>
+        <div className='w-[98vw] lg:w-[400px] bg-white/95 rounded-2xl border shadow-xl flex flex-col items-center'>
             <div className='title my-20'>Sign In</div>
             <div className='w-[95%] flex flex-col items-center gap-5'>
                 <input className='border p-1 w-full' onChange={(e) => {setEmail(e.target.value);}} type='email' placeholder='Email ...' />
                 <input className='border p-1 w-full' onChange={(e) => {setPassword(e.target.value);}} type='password' placeholder='Password ...' />
             </div>
-            <button className='subtitle font-bold text-white w-[95%] bg-gradient-to-r from-purple-700 to-red-600 my-10 py-2' onClick={handleSignin}>Sign In</button>
+            <button className='subtitle font-bold text-white w-[95%] bg-gradient-to-r from-purple-900 to-red-800 my-10 py-2 rounded-xl' onClick={handleSignin}>Sign In</button>
         </div>
     </div>
   )
